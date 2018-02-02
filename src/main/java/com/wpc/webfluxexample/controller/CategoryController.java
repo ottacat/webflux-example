77073176e29c2dda6483e13c98e3e6718e81d6da
Tim-THINK\Tim <timothyshouldice@gmail.com>
2018-02-01 19:40:46 -0500

    Addition of POST method to Category controller and associated test.

--- src/main/java/com/wpc/webfluxexample/controller/CategoryController.java
+++ src/main/java/com/wpc/webfluxexample/controller/CategoryController.java
@@ -2,9 +2,9 @@ package com.wpc.webfluxexample.controller;
 
 import com.wpc.webfluxexample.domain.Category;
 import com.wpc.webfluxexample.repository.CategoryRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.reactivestreams.Publisher;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -25,5 +25,11 @@ public class CategoryController {
     @GetMapping("/api/v1/categories/{id}")
     Mono<Category> getById(@PathVariable String id) {
         return categoryRepository.findById(id);
+    }
+
+    @PostMapping("/api/v1/categories")
+    @ResponseStatus(HttpStatus.CREATED)
+    Mono<Void> create(@RequestBody Publisher<Category> categoryStream) {
+        return categoryRepository.saveAll(categoryStream).then();
     }
 }
