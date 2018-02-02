546b918337eba011a7d25c5ece02c010ec74d1c2
Tim-THINK\Tim <timothyshouldice@gmail.com>
2018-02-01 19:16:08 -0500

    Implementation of Vendor Controller and unit tests.

---
+++ src/test/java/com/wpc/webfluxexample/controller/VendorControllerTest.java
@@ -0,0 +1,49 @@
+package com.wpc.webfluxexample.controller;
+
+import com.wpc.webfluxexample.domain.Vendor;
+import com.wpc.webfluxexample.repository.VendorRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class VendorControllerTest {
+
+    WebTestClient webTestClient;
+    VendorRepository vendorRepository;
+    VendorController vendorController;
+
+    @Before
+    public void setUp() throws Exception {
+        vendorRepository = Mockito.mock(VendorRepository.class);
+        vendorController = new VendorController(vendorRepository);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+    }
+
+    @Test
+    public void list() {
+        BDDMockito.given(vendorRepository.findAll())
+                .willReturn(Flux.just(Vendor.builder().firstName("Joe").lastName("Blow").build(),
+                        Vendor.builder().firstName("John").lastName("Smith").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    public void getById() {
+        BDDMockito.given(vendorRepository.findById("someid"))
+                .willReturn(Mono.just(Vendor.builder().firstName("John").lastName("Smith").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendors/someid")
+                .exchange()
+                .expectBody(Vendor.class);
+    }
+}
